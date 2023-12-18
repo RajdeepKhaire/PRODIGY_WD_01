8c8cc29095a3aa81700250a1eb1a9d402f16c530
RajdeepKhaire <154271559+RajdeepKhaire@users.noreply.github.com>
2023-12-19 00:45:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var navbar = document.getElementById("navbar");
+
+    // Change style on scroll
+    window.onscroll = function () {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            navbar.style.backgroundColor = "#555";
+        } else {
+            navbar.style.backgroundColor = "#333";
+        }
+    };
+});
